a69310d7fa7a5506b6e1d5023405939c9c3ffb4c
KudneuS <161035306+KudneuS@users.noreply.github.com>
2024-03-16 20:53:43 +0200

    Add files via upload

--- js/MenuManager.js
+++ js/MenuManager.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     AddItemToStorage("editPatternBurg", false);
 
     $("#privatePatterns").html(GetItemFromStorage("patternsBurg"));
-    $(".deleteButton").html('добавить в корзину').removeAttr("class", "deleteButton").attr("class", "patternButton addToCart trn");
+    $(".deleteButton").text('добавить в корзину').removeAttr("class", "deleteButton").removeAttr("data-trn-key").attr("class", "patternButton addToCart trn");
     
     if($("#privatePatterns").children().length == 0){
         $("#privatePatterns").html($(NothingHereText).attr("style", "height: 12em;"));
